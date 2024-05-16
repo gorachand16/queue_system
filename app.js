d051f7be0d5bac04e05b13031e1499d22e4d046f
gorachand16 <55240224+gorachand16@users.noreply.github.com>
2024-05-16 14:46:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Import required modules
 const express = require('express');
 const userRouter =require("./api/patients/patient_router");
-// Initialize Express app1
+// Initialize Express app
 const app = express();
 const PORT = 3000; // Choose a port for your server
 app.use("/patients", userRouter);
