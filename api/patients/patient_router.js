9865d8026838214cc92cee5724e723de2ff7cd39
shaunakGhosh <shaunak.ghosh@cbnits.com>
2024-05-16 18:20:28 +0530

    GetserialNumber API done

--- api/patients/patient_router.js
+++ api/patients/patient_router.js
@@ -6,23 +6,94 @@ module.exports = router;
 router.use(bodyParser.json());
 router.post('/GetUserDetailsByNRICNumber', async (req, res) => {
     try {
-       const body = req.body
+        const body = req.body
         // Query to fetch data from PostgreSQL based on the input ID
-        if (body && body.nric_number) {
-            const query = 'SELECT * FROM public.patient p WHERE p.nric_number = $1';
-            const { rows } = await pool.query(query, [body.nric_number]);
-            if (rows.length === 0) {
-                // If no data is found for the given ID, return 404 Not Found
-                res.status(404).json({ error: 'Data not found' });
-            } else {
-                res.json(rows);
-            }
-        } else {
+        if (!body && !body.nric_number) {
             res.status(400).json({ error: 'Invalid request body or missing nric_number' });
         }
-       
-    }  catch (err) {
+        const query = 'SELECT * FROM public.patient p WHERE p.nric_number = $1';
+        const { rows } = await pool.query(query, [body.nric_number]);
+        if (rows.length === 0) {
+            // If no data is found for the given ID, return 404 Not Found
+            res.status(404).json({ error: 'patient not found' });
+        } else {
+            res.json(rows);
+        }
+    } catch (err) {
         console.error('Error fetching data', err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+router.post('/GetserialNumber', async (req, res) => {
+    try {
+        const body = req.body
+        if (!body && !body.nric_number && !body.queue_array) {
+            res.status(400).json({ error: 'Invalid request body or missing nric_number' });
+        }
+        const query = 'SELECT p.id FROM public.patient p WHERE p.nric_number = $1';
+        const { rows } = await pool.query(query, [body.nric_number]);
+        if (rows.length === 0) {
+            res.status(404).json({ error: 'patient not found' });
+        }
+        const id = rows[0].id
+        var insertquery = `INSERT INTO public.appointment (nric_number, patient_id, appointment_date, `;
+        var values = `'${body.nric_number}', '${id}', now(), `;
+        for (var i = 0; i < body.queue_array.length; i++) {
+            if (body.queue_array[i] == 'consultation') {
+                var strId = (i + 1).toString();
+                if (i == 0) {
+                    insertquery += `appointment_consultation_status, consultation_priority, `;
+                    values += `'pending', '${strId}', `;
+                } else {
+                    insertquery += `appointment_consultation_status, consultation_priority, `;
+                    values += `'init', '${strId}', `;
+                }
+            }
+            if (body.queue_array[i] == 'labtest') {
+                var strId = (i + 1).toString();
+                if (i == 0) {
+                    insertquery += `appointment_lab_test_status, lab_test_priority, `;
+                    values += `'pending', '${strId}', `;
+                } else {
+                    insertquery += `appointment_lab_test_status, lab_test_priority, `;
+                    values += `'init', '${strId}', `;
+                }
+            }
+            if (body.queue_array[i] == 'medicine') {
+                var strId = (i + 1).toString();
+                if (i == 0) {
+                    insertquery += `appointment_medicine_status, medicine_priority, `;
+                    values += `'pending', '${strId}', `;
+                } else {
+                    insertquery += `appointment_medicine_status, medicine_priority, `;
+                    values += `'init', '${strId}', `;
+                }
+            }
+            if (body.queue_array[i] == 'payment') {
+                var strId = (i + 1).toString();
+                if (i == 0) {
+                    insertquery += `appointment_payment_status, payment_priority, `;
+                    values += `'pending', '${strId}', `;
+                } else {
+                    insertquery += `appointment_payment_status, payment_priority, `;
+                    values += `'init', '${strId}', `;
+                }
+            }
+        }
+        values += `now(), now(), now()`;
+        insertquery += `booking_date, created_at, updated_at) VALUES (${values}) RETURNING id as serial_number;`;
+        console.log(insertquery)
+        const { rows:resultrows } = await pool.query(insertquery);
+        const serialNumber = resultrows[0].serial_number; 
+
+        var queueQuery = `INSERT INTO public.${body.queue_array[0]}_queue
+        ( appointment_id, status, ${body.queue_array[0]}_date, created_at, updated_at)
+        VALUES( ${serialNumber}, 'active'::character varying, now(), now(), now());`
+        await pool.query(queueQuery)
+        res.json({ serialNumber});
+    } catch (err) {
+        console.error('Error fetching data', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
--- schema.sql
+++ schema.sql
@@ -31,3 +31,64 @@ VALUES
     ('S2756245O', 'Sarah Taylor', '505 Walnut St, Manor', '567-890-1234', 'sarah.taylor@example.com', '27', '1997-04-05'),
     ('S2756246P', 'Matthew Anderson', '606 Spruce St, Park', '678-901-2345', 'matthew.anderson@example.com', '38', '1986-11-28'),
     ('S2756247Q', 'Amanda Garcia', '707 Cedar St, Estate', '789-012-3456', 'amanda.garcia@example.com', '31', '1993-08-17');
+
+
+create table public.appointment (
+id serial primary key,
+nric_number varchar null, 
+patient_id int null,
+appointment_date timestamp  default now(),
+appointment_details jsonb null,
+appointment_consultation_status varchar default null,
+consultation_priority int default 0,
+appointment_medicine_status varchar default null,
+medicine_priority int default 0,
+appointment_lab_test_status varchar default null,
+lab_test_priority int default 0,
+appointment_payment_status varchar default null,
+payment_priority int default 0,
+service_priority jsonb null,
+booking_date timestamp  default now(),
+created_at timestamp  default now(),
+updated_at timestamp  default now()
+)
+alter table public.appointment drop column appointment_details
+alter table public.appointment drop column service_priority
+
+
+
+create table public.consultation_queue (
+id serial primary key,
+appointment_id int null,
+status varchar default 'active',
+consultation_date timestamp  default now(),
+created_at timestamp  default now(),
+updated_at timestamp  default now()
+) 
+
+create table public.medicine_queue (
+id serial primary key,
+appointment_id int null,
+status varchar default 'active',
+medicine_date timestamp  default now(),
+created_at timestamp  default now(),
+updated_at timestamp  default now()
+) 
+
+create table public.labtest_queue (
+id serial primary key,
+appointment_id int null,
+status varchar default 'active',
+labtest_date timestamp  default now(),
+created_at timestamp  default now(),
+updated_at timestamp  default now()
+) 
+
+create table public.payment_queue (
+id serial primary key,
+appointment_id int null,
+status varchar default 'active',
+payment_date timestamp  default now(),
+created_at timestamp  default now(),
+updated_at timestamp  default now()
+)
